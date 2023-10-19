a078b04bd6517237eccd3f87d4d56fdc8f5290ee
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2023-10-19 10:32:09 +0100

    application finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './application/home/home.component';
+import { ListproductsComponent } from './application/listproducts/listproducts.component';
+import { ContactComponent } from './application/contact/contact.component';
+import { MenuComponent } from './application/menu/menu.component';
+import { SelectedproductComponent } from './application/selectedproduct/selectedproduct.component';
+import { Error404Component } from './application/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListproductsComponent,
+    ContactComponent,
+    MenuComponent,
+    SelectedproductComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
